6a99d5dbeba5fbc35af9a93ee2f555517bae7691
Timothee GROLLIER <timotheegrollier@gmail.com>
2021-07-16 17:16:06 +0200

    reHerok

--- routes/postsController.js
+++ routes/postsController.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const ObjectID = require('mongoose').Types.ObjectId;
 
-const {PostsModel} = require('../models/postsModel');
+const {PostsModel} = require('/models/postsModel');
 
 
 router.get('/', (req,res)=>{
